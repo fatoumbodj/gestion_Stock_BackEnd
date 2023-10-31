d15ebf991594b52d0a643f32c06bd47b8e97cf77
fatou mbodj <mbodjfaticha99@gmail.com>
2023-10-31 22:10:17 +0000

    initialize

---
+++ src/main/java/com/gestionDeStock/backEnd/backend/GestionDeStock/BackendGestionDeStockApplication.java
@@ -0,0 +1,13 @@
+package com.gestionDeStock.backEnd.backend.GestionDeStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGestionDeStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGestionDeStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionDeStock/backEnd/backend/GestionDeStock/BackendGestionDeStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionDeStock.backEnd.backend.GestionDeStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGestionDeStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
